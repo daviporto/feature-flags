8f7aaf4c1453ddc0ccda10578f590e2a78702d22
daviporto <portodaviporto@gmail.com>
2025-10-22 18:33:17 -0300

    implement ff output mapper test

--- back/src/feature-flag/application/dtos/__tests__/unit/feature-flag-output.spec.ts
+++ back/src/feature-flag/application/dtos/__tests__/unit/feature-flag-output.spec.ts
@@ -1,7 +1,14 @@
 import { FeatureFlagEntity } from '@/feature-flag/domain/entities/feature-flag.entity';
-import { FeatureFlagMapper } from '@/feature-flag/application/dtos/feature-flag-output';
+import {FeatureFlagMapper, FeatureFlagOutputMapper} from '@/feature-flag/application/dtos/feature-flag-output';
+import {FeatureFlagDataBuilder} from "@/feature-flag/domain/testing/helper/feature-flag-data-builder";
 
 describe('FeatureFlag output unit tests', () => {
-  it('should convert a featureflag in output', () => {
+  it('should convert a feature flag in output', () => {
+    const feature =  new FeatureFlagEntity(FeatureFlagDataBuilder({}));
+    const spyJson = jest.spyOn(feature, 'toJSON');
+    const sut = FeatureFlagOutputMapper.toOutput(feature);
+
+    expect(spyJson).toHaveBeenCalled();
+    expect(sut).toStrictEqual(feature.toJSON());
   });
 });
