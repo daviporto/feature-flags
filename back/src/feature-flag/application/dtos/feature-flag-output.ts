a8864fb9cd1a5916afd225caa563179247163c4c
daviporto <portodaviporto@gmail.com>
2025-10-22 18:19:05 -0300

    add feature to feature flag output

--- back/src/feature-flag/application/dtos/feature-flag-output.ts
+++ back/src/feature-flag/application/dtos/feature-flag-output.ts
@@ -1,11 +1,16 @@
-import { FeatureFlagEntity } from '@/feature-flag/domain/entities/feature-flag.entity';
+import {FeatureFlagEntity} from '@/feature-flag/domain/entities/feature-flag.entity';
 
 export type FeatureFlagOutput = {
-
+    name: string;
+    description: string;
+    enabled: boolean;
+    userId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 export class FeatureFlagOutputMapper {
-  static toOutput(entity: FeatureFlagEntity): FeatureFlagOutput {
-    return entity.toJSON();
-  }
+    static toOutput(entity: FeatureFlagEntity): FeatureFlagOutput {
+        return entity.toJSON();
+    }
 }
