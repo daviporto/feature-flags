487a7bff996d761dc0fb07e86e16bdf16946c999
daviporto <portodaviporto@gmail.com>
2025-10-22 18:19:30 -0300

    implement ff presenter

--- back/src/feature-flag/infrastructure/presenters/feature-flag.presenter.ts
+++ back/src/feature-flag/infrastructure/presenters/feature-flag.presenter.ts
@@ -1,24 +1,52 @@
-import { FeatureFlagOutput } from '@/feature-flag/application/dtos/feature-flag-output';
-import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter';
-import { ListFeatureFlagsUsecase } from '@/feature-flag/application/usecases/list-feature-flags.usecase';
-import { ApiProperty, ApiExtraModels } from '@nestjs/swagger';
+import {FeatureFlagOutput} from '@/feature-flag/application/dtos/feature-flag-output';
+import {CollectionPresenter} from '@/shared/infrastructure/presenters/collection.presenter';
+import {ListFeatureFlagsUsecase} from '@/feature-flag/application/usecases/list-feature-flags.usecase';
+import {ApiProperty, ApiExtraModels} from '@nestjs/swagger';
+import {Transform} from "class-transformer";
 
 export class FeatureFlagPresenter {
-  constructor(output: FeatureFlagOutput) { }
+    @ApiProperty({description: 'The id of the feature flag'})
+    id: string;
+
+    @ApiProperty({description: 'The name of the feature flag'})
+    name: string
+
+    @ApiProperty({description: 'The description of the feature flag'})
+    description: string
+
+    @ApiProperty({description: 'The status of the feature flag, if is active or not'})
+    enabled: boolean;
+
+    @ApiProperty({description: 'The date when the feature flag was created'})
+    @Transform(({value}: { value: Date }) => value.toISOString())
+    createdAt: Date;
+
+    @ApiProperty({nullable, description: 'The data of last feature flag update'})
+    @Transform(({value}: { value: Date }) => value.toISOString())
+
+
+    constructor(output: FeatureFlagOutput) {
+        this.id = output.id;
+        this.name = output.name;
+        this.description = output.description;
+        this.enabled = output.enabled;
+        this.createdAt = output.createdAt;
+        this.updatedAt = output.updatedAt;
+    }
 }
 
 @ApiExtraModels(FeatureFlagPresenter)
 export class FeatureFlagCollectionPresenter extends CollectionPresenter {
-  @ApiProperty({
-    type: FeatureFlagPresenter,
-    isArray: true,
-    description: 'List of FeatureFlag',
-  })
-  data: FeatureFlagPresenter[];
+    @ApiProperty({
+        type: FeatureFlagPresenter,
+        isArray: true,
+        description: 'List of FeatureFlags',
+    })
+    data: FeatureFlagPresenter[];
 
-  constructor(output: ListFeatureFlagsUsecase.Output) {
-    const { items, ...pagination } = output;
-    super(pagination);
-    this.data = items.map((item) => new FeatureFlagPresenter(item));
-  }
+    constructor(output: ListFeatureFlagsUsecase.Output) {
+        const {items, ...pagination} = output;
+        super(pagination);
+        this.data = items.map((item) => new FeatureFlagPresenter(item));
+    }
 }
