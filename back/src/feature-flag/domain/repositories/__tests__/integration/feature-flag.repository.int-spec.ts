f5e90083296ac13e8021c9ee677e64c22df8bd63
daviporto <portodaviporto@gmail.com>
2025-10-22 19:44:51 -0300

    finish list feature flag int tests

--- back/src/feature-flag/domain/repositories/__tests__/integration/feature-flag.repository.int-spec.ts
+++ back/src/feature-flag/domain/repositories/__tests__/integration/feature-flag.repository.int-spec.ts
@@ -1,4 +1,7 @@
-import { setUpPrismaTest } from '@/shared/infrastructure/database/prisma/testing/set-up-prisma-test';
+import {
+  resetDatabase,
+  setUpPrismaTest,
+} from '@/shared/infrastructure/database/prisma/testing/set-up-prisma-test';
 import { FeatureFlagDataBuilder } from '@/feature-flag/domain/testing/helper/feature-flag-data-builder';
 import { PrismaClient } from '@prisma/client';
 import { FeatureFlagPrismaRepository } from '@/feature-flag/infrastructure/database/prisma/repositories/feature-flag-prisma.repository';
@@ -6,9 +9,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
 import { FeatureFlagEntity } from '@/feature-flag/domain/entities/feature-flag.entity';
 import { faker } from '@faker-js/faker';
-import { FeatureFlagRepository } from '@/feature-flag/domain/repositories/feature-flag.repository';
-import { SortOrderEnum } from '@/shared/domain/repositories/searchable-repository-contracts';
 import { FeatureFlagWithIdNotFoundError } from '@/feature-flag/infrastructure/errors/feature-flag-with-id-not-found-error';
+import { UserPrismaTestingHelper } from '@/user/infrastructure/database/prisma/testing/user-prisma.testing-helper';
+import { FeatureFlagPrismaTestingHelper } from '@/feature-flag/infrastructure/database/prisma/testing/feature-flag-prisma.testing-helper';
 
 describe('FeatureFlag prisma repository integration tests', () => {
   const prismaService = new PrismaClient();
@@ -25,12 +28,14 @@ describe('FeatureFlag prisma repository integration tests', () => {
 
   beforeEach(async () => {
     sut = new FeatureFlagPrismaRepository(prismaService as any);
-    await prismaService.feature-flag.deleteMany();
+    await resetDatabase(prismaService);
   });
 
   afterAll(async () => {
     await prismaService.$disconnect();
     await module.close();
+
+    await resetDatabase(prismaService);
   });
 
   it('should throw error when entity does not exist', () => {
@@ -39,47 +44,63 @@ describe('FeatureFlag prisma repository integration tests', () => {
     );
   });
 
-  it('should find feature-flag by id', async () => {
-    const entity = new FeatureFlagEntity(FeatureFlagDataBuilder({}));
+  it('should find feature flag by id', async () => {
+    const user =
+      await UserPrismaTestingHelper.createUserAsEntity(prismaService);
+    const entity = new FeatureFlagEntity(
+      FeatureFlagDataBuilder({
+        userId: user.id,
+      }),
+    );
 
-    const createdFeatureFlag = await prismaService.feature-flag.create({
+    const createdFeatureFlag = await prismaService.featureFlag.create({
       data: entity.toJSON(),
     });
 
-    const feature-flag = await sut.findById(createdFeatureFlag.id);
+    const featureFlag = await sut.findById(createdFeatureFlag.id);
 
     expect(sut).not.toBeNull();
-    expect(feature-flag.toJSON()).toStrictEqual(entity.toJSON());
+    expect(featureFlag.toJSON()).toStrictEqual(entity.toJSON());
   });
 
-  it('should insert a new feature-flag', async () => {
-    const entity = new FeatureFlagEntity(FeatureFlagDataBuilder({}));
+  it('should insert a new feature flag', async () => {
+    const user =
+      await UserPrismaTestingHelper.createUserAsEntity(prismaService);
+    const entity = new FeatureFlagEntity(
+      FeatureFlagDataBuilder({
+        userId: user.id,
+      }),
+    );
     await sut.insert(entity);
-
   });
 
-  it('should return one feature-flag if theres only one with find all', async () => {
-    const entity = new FeatureFlagEntity(FeatureFlagDataBuilder({}));
-    await sut.insert(entity);
+  it('should return one feature flag if theres only one with find all', async () => {
+    const entity =
+      await FeatureFlagPrismaTestingHelper.createFeatureFlagAsEntity(
+        prismaService,
+      );
 
-    const feature-flags = await sut.findAll();
+    const featureFlags = await sut.findAll();
 
-    expect(feature-flags).toHaveLength(1);
-    expect(feature-flags[0].toJSON()).toStrictEqual(entity.toJSON());
+    expect(featureFlags).toHaveLength(1);
+    expect(featureFlags[0].toJSON()).toStrictEqual(entity.toJSON());
   });
 
-  it('should throw error when trying to update non-existent featureflag', async () => {
+  it('should throw error when trying to update non-existent feature flag', async () => {
     const nonExistentId = faker.string.uuid();
-    const entity = new FeatureFlagEntity(FeatureFlagDataBuilder({}), nonExistentId);
+    const entity = new FeatureFlagEntity(
+      FeatureFlagDataBuilder({}),
+      nonExistentId,
+    );
 
     await expect(sut.update(entity)).rejects.toThrow(
       new FeatureFlagWithIdNotFoundError(nonExistentId),
     );
   });
 
-  it('should update a featureflag successfully', async () => { });
+  it('should update a feature flag successfully', async () => {});
 
-  it('should throw error when trying to delete non-existent featureflag', async () => {
+  it('should throw error when trying to delete non-existent feature flag', async () => {
     const nonExistentId = faker.string.uuid();
 
     await expect(sut.delete(nonExistentId)).rejects.toThrow(
@@ -87,23 +108,18 @@ describe('FeatureFlag prisma repository integration tests', () => {
     );
   });
 
-  it('should delete a featureflag successfully', async () => {
-    const entity = new FeatureFlagEntity(FeatureFlagDataBuilder({ name: 'John' }));
-    await sut.insert(entity);
+  it('should delete a feature flag successfully', async () => {
+    const entity =
+      await FeatureFlagPrismaTestingHelper.createFeatureFlagAsEntity(
+        prismaService,
+      );
 
     await sut.delete(entity.id);
 
-    const feature-flagCount = await prismaService.feature-flag.count({
+    const featureFlagCount = await prismaService.featureFlag.count({
       where: { id: entity.id },
     });
 
-    expect(feature-flagCount).toBe(0);
-  });
-
-
-  describe('search tests', () => {
-    it.todo('should return with default values', async () => { });
-
-    it.todo('should paginate feature-flags', async () => { });
+    expect(featureFlagCount).toBe(0);
   });
 });
