7acc8a5fa43631721e5279f51d652d7b2cca9e19
daviporto <portodaviporto@gmail.com>
2025-10-22 18:55:14 -0300

    finish implementing feature flag delete usecase unit tests

--- back/src/feature-flag/application/usecases/__tests__/unit/delete-feature-flag.usecase.spec.ts
+++ back/src/feature-flag/application/usecases/__tests__/unit/delete-feature-flag.usecase.spec.ts
@@ -1,38 +1,39 @@
-import { FeatureFlagInMemoryRepository } from '@/feature-flag/infrastructure/database/in-memory/repositories/feature-flag-in-memory.repository';
-import { GetFeatureFlagUsecase } from '@/feature-flag/application/usecases/get-feature-flag.usecase';
-import { FeatureFlagEntity } from '@/feature-flag/domain/entities/feature-flag.entity';
-import { DeleteFeatureFlagUsecase } from '@/feature-flag/application/usecases/delete-feature-flag.usecase';
+import {
+  FeatureFlagInMemoryRepository
+} from '@/feature-flag/infrastructure/database/in-memory/repositories/feature-flag-in-memory.repository';
+import {FeatureFlagEntity} from '@/feature-flag/domain/entities/feature-flag.entity';
+import {DeleteFeatureFlagUsecase} from '@/feature-flag/application/usecases/delete-feature-flag.usecase';
 
-describe('Delete featureflag use case test', () => {
-  let sut: DeleteFeatureFlagUsecase.UseCase;
-  let repository: FeatureFlagInMemoryRepository;
+describe('Delete feature flag use case test', () => {
+    let sut: DeleteFeatureFlagUsecase.UseCase;
+    let repository: FeatureFlagInMemoryRepository;
 
-  beforeEach(() => {
-    repository = new FeatureFlagInMemoryRepository();
-    sut = new DeleteFeatureFlagUsecase.UseCase(repository);
-  });
+    beforeEach(() => {
+        repository = new FeatureFlagInMemoryRepository();
+        sut = new DeleteFeatureFlagUsecase.UseCase(repository);
+    });
 
-  it('should throw exception if featureflag does not exist', async () => {
-    const input = { id: 'non-existent-id' };
+    it('should throw exception if feature flag does not exist', async () => {
+        const input = {id: 'non-existent-id'};
 
-    await expect(sut.execute(input)).rejects.toThrow(
-    );
-  });
+        await expect(sut.execute(input)).rejects.toThrow(
+        );
+    });
 
 
-  it('should call repository delete with correct ID', async () => {
-    const feature_flag = new FeatureFlagEntity();
-    await repository.insert(feature_flag);
+    it('should call repository delete with correct ID', async () => {
+        const feature_flag = new FeatureFlagEntity();
+        await repository.insert(feature_flag);
 
-    const spyDelete = jest.spyOn(repository, 'delete');
+        const spyDelete = jest.spyOn(repository, 'delete');
 
-    const input = { id: feature_flag.id };
+        const input = {id: feature_flag.id};
 
-    expect(repository.items).toHaveLength(1)
-    await sut.execute(input);
+        expect(repository.items).toHaveLength(1)
+        await sut.execute(input);
 
-    expect(spyDelete).toHaveBeenCalledWith(feature_flag.id);
+        expect(spyDelete).toHaveBeenCalledWith(feature_flag.id);
 
-    expect(repository.items).toHaveLength(0)
-  });
+        expect(repository.items).toHaveLength(0)
+    });
 });
