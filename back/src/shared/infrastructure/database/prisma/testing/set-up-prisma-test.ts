386990ab36521c9ae7e306fec0b434a228b4fead
daviporto <portodaviporto@gmail.com>
2025-10-22 19:42:58 -0300

    add resetDatabase in setUp prisma

--- back/src/shared/infrastructure/database/prisma/testing/set-up-prisma-test.ts
+++ back/src/shared/infrastructure/database/prisma/testing/set-up-prisma-test.ts
@@ -1,5 +1,12 @@
 import { execSync } from 'node:child_process';
+import { PrismaClient } from '@prisma/client';
 
 export function setUpPrismaTest() {
   execSync('npm run prisma:migrate-test');
+}
+
+export async function resetDatabase(prismaService: PrismaClient) {
+  await prismaService.$executeRaw`TRUNCATE TABLE 
+    users, feature_flags, user_feature_flags, audit_logs
+    CASCADE`;
 }
