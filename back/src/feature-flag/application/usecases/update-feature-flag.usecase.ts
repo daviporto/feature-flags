2bbd9f2b3a0fb89279b3a62f35cfc43cadcdf8ec
davi <davi.araujo@onfly.com.br>
2025-10-26 16:12:14 -0300

    finish update ff usecase

--- back/src/feature-flag/application/usecases/update-feature-flag.usecase.ts
+++ back/src/feature-flag/application/usecases/update-feature-flag.usecase.ts
@@ -4,16 +4,52 @@ import {
   FeatureFlagOutputMapper,
 } from '@/feature-flag/application/dtos/feature-flag-output';
 import { UseCaseInterface } from '@/shared/application/use-cases/use-case';
-import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+import { AbstractUseCase } from '@/shared/application/use-cases/abstract-use-case';
+import { isUUID } from 'class-validator';
+import { InvalidUuidError } from '@/shared/application/errors/invalid-uuid-error';
 
 export namespace UpdateFeatureFlagUsecase {
-  export type Input = { };
+  export type Input = {
+    id: string;
+    name: string;
+    description: string;
+    enabled: boolean;
+  };
 
   export type Output = FeatureFlagOutput;
 
-  export class UseCase implements UseCaseInterface<Input, Output> {
-    constructor(private repository: FeatureFlagRepository.Repository) {}
+  export class UseCase
+    extends AbstractUseCase<Input, Output>
+    implements UseCaseInterface<Input, Output>
+  {
+    constructor(private repository: FeatureFlagRepository.Repository) {
+      super();
+    }
 
-    async execute(input: Input): Promise<Output> { }
+    async execute(input: Input): Promise<Output> {
+      this.assureRequiredInputProvided(input);
+
+      if (!isUUID(input.id)) {
+        throw new InvalidUuidError(input.id);
+      }
+
+      await this.repository.assureFeatureFlagExists(input.id);
+
+      const featureFlag = await this.repository.findById(input.id);
+
+      featureFlag.update(input.name, input.description, input.enabled);
+
+      await this.repository.update(featureFlag);
+
+      const updatedFeatureFlag = await this.repository.findById(input.id);
+
+      return FeatureFlagOutputMapper.toOutput(updatedFeatureFlag);
+    }
+
+    protected assureRequiredInputProvided(input: Input) {
+      const requiredFields = ['id', 'name', 'description', 'enabled'];
+
+      super.assureRequiredInputProvided(input, requiredFields);
+    }
   }
 }
