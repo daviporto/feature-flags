23c258b78a4fde2f241b48e012a7750f4db208b8
davi <davi.araujo@onfly.com.br>
2025-10-26 15:02:48 -0300

    adjust data generator to not override false booleans prop

--- back/src/feature-flag/domain/testing/helper/feature-flag-data-builder.ts
+++ back/src/feature-flag/domain/testing/helper/feature-flag-data-builder.ts
@@ -1,11 +1,16 @@
 import { faker } from '@faker-js/faker';
 import { FeatureFlagProps } from '@/feature-flag/domain/entities/feature-flag.entity';
+import { isUndefined } from '@nestjs/common/utils/shared.utils';
 
 export function FeatureFlagDataBuilder(props: Partial<FeatureFlagProps>) {
+  if (isUndefined(props.enabled)) {
+    props.enabled = faker.datatype.boolean();
+  }
+
   return {
     name: props.name || faker.string.alphanumeric(),
     description: props.description || faker.string.alphanumeric(),
-    enabled: props.enabled || faker.datatype.boolean(),
+    enabled: props.enabled,
     userId: props.userId || faker.string.uuid(),
     createdAt: props.createdAt || new Date(),
     updatedAt: props.updatedAt || new Date(),
