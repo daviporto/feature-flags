8962d4b28e617df5f401b87478df05f97d8446b1
davi <davi.araujo@onfly.com.br>
2025-10-26 15:03:15 -0300

    fix formatting

--- back/src/feature-flag/application/dtos/feature-flag-output.ts
+++ back/src/feature-flag/application/dtos/feature-flag-output.ts
@@ -1,17 +1,17 @@
-import {FeatureFlagEntity} from '@/feature-flag/domain/entities/feature-flag.entity';
+import { FeatureFlagEntity } from '@/feature-flag/domain/entities/feature-flag.entity';
 
 export type FeatureFlagOutput = {
-    id: string;
-    name: string;
-    description: string;
-    enabled: boolean;
-    userId: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
+  id: string;
+  name: string;
+  description: string;
+  enabled: boolean;
+  userId: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
 
 export class FeatureFlagOutputMapper {
-    static toOutput(entity: FeatureFlagEntity): FeatureFlagOutput {
-        return entity.toJSON();
-    }
+  static toOutput(entity: FeatureFlagEntity): FeatureFlagOutput {
+    return entity.toJSON();
+  }
 }
