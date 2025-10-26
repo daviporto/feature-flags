0fe84be13119e272c007965a9a386b5e9c2b50b2
davi <davi.araujo@onfly.com.br>
2025-10-26 16:10:38 -0300

    finish ff controller and its tests

--- back/src/feature-flag/infrastructure/feature-flag.controller.ts
+++ back/src/feature-flag/infrastructure/feature-flag.controller.ts
@@ -1,95 +1,116 @@
-import {Body, Controller, Delete, Get, HttpCode, Inject, Param, Put, Query, UseGuards,} from '@nestjs/common';
-import {UpdateFeatureFlagUsecase} from '@/feature-flag/application/usecases/update-feature-flag.usecase';
-import {GetFeatureFlagUsecase} from '@/feature-flag/application/usecases/get-feature-flag.usecase';
-import {ListFeatureFlagsUsecase} from '@/feature-flag/application/usecases/list-feature-flags.usecase';
-import {DeleteFeatureFlagUsecase} from '@/feature-flag/application/usecases/delete-feature-flag.usecase';
-import {ListFeatureFlagsDto} from '@/feature-flag/infrastructure/dtos/list-feature-flags.dto';
-import {UpdateFeatureFlagDto} from '@/feature-flag/infrastructure/dtos/update-feature-flag.dto';
-import {FeatureFlagOutput} from '@/feature-flag/application/dtos/feature-flag-output';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Inject,
+  Param,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { UpdateFeatureFlagUsecase } from '@/feature-flag/application/usecases/update-feature-flag.usecase';
+import { GetFeatureFlagUsecase } from '@/feature-flag/application/usecases/get-feature-flag.usecase';
+import { DeleteFeatureFlagUsecase } from '@/feature-flag/application/usecases/delete-feature-flag.usecase';
+import { UpdateFeatureFlagDto } from '@/feature-flag/infrastructure/dtos/update-feature-flag.dto';
+import { FeatureFlagOutput } from '@/feature-flag/application/dtos/feature-flag-output';
 import {
   FeatureFlagCollectionPresenter,
   FeatureFlagPresenter,
 } from '@/feature-flag/infrastructure/presenters/feature-flag.presenter';
-import {AuthService} from '@/auth/infrastructure/auth.service';
-import {AuthGuard} from '@/auth/infrastructure/auth.guard';
-import {ApiBearerAuth, ApiOkResponse, ApiResponse, ApiTags,} from '@nestjs/swagger';
+import { AuthService } from '@/auth/infrastructure/auth.service';
+import { AuthGuard } from '@/auth/infrastructure/auth.guard';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ListFeatureFlagsUsecase } from '@/feature-flag/application/usecases/list-feature-flag.usecase';
+import { ListFeatureFlagsDto } from '@/feature-flag/infrastructure/dtos/list-feature-flag.dto';
 
 @ApiTags('feature-flag')
 @Controller('feature-flag')
 export class FeatureFlagController {
-    @Inject(UpdateFeatureFlagUsecase.UseCase)
-    private updateFeatureFlagUseCase: UpdateFeatureFlagUsecase.UseCase;
+  @Inject(UpdateFeatureFlagUsecase.UseCase)
+  private updateFeatureFlagUseCase: UpdateFeatureFlagUsecase.UseCase;
 
-    @Inject(GetFeatureFlagUsecase.UseCase)
-    private getFeatureFlagUseCase: GetFeatureFlagUsecase.UseCase;
+  @Inject(GetFeatureFlagUsecase.UseCase)
+  private getFeatureFlagUseCase: GetFeatureFlagUsecase.UseCase;
 
-    @Inject(ListFeatureFlagsUsecase.UseCase)
-    private listFeatureFlagsUseCase: ListFeatureFlagsUsecase.UseCase;
+  @Inject(ListFeatureFlagsUsecase.UseCase)
+  private listFeatureFlagsUseCase: ListFeatureFlagsUsecase.UseCase;
 
-    @Inject(DeleteFeatureFlagUsecase.UseCase)
-    private deleteFeatureFlagUseCase: DeleteFeatureFlagUsecase.UseCase;
+  @Inject(DeleteFeatureFlagUsecase.UseCase)
+  private deleteFeatureFlagUseCase: DeleteFeatureFlagUsecase.UseCase;
 
-    @Inject(AuthService)
-    private authService: AuthService;
+  @Inject(AuthService)
+  private authService: AuthService;
 
-    static featureFlagToResponse(output: FeatureFlagOutput): FeatureFlagPresenter {
-        return new FeatureFlagPresenter(output);
-    }
+  static featureFlagToResponse(
+    output: FeatureFlagOutput,
+  ): FeatureFlagPresenter {
+    return new FeatureFlagPresenter(output);
+  }
 
-    static listFeatureFlagToResponse(
-        output: ListFeatureFlagsUsecase.Output,
-    ): FeatureFlagCollectionPresenter {
-        return new FeatureFlagCollectionPresenter(output);
-    }
+  static listFeatureFlagToResponse(
+    output: ListFeatureFlagsUsecase.Output,
+  ): FeatureFlagCollectionPresenter {
+    return new FeatureFlagCollectionPresenter(output);
+  }
 
-    @ApiOkResponse({
-        description: 'The FeatureFlag has been successfully created.',
-        type: FeatureFlagCollectionPresenter,
-    })
-    @ApiResponse({status: 422, description: 'Unprocessable Entity'})
-    @ApiResponse({status: 401, description: 'Unauthorized'})
-    @UseGuards(AuthGuard)
-    @Get()
-    async search(@Query() searchParams: ListFeatureFlagsDto) {
-        const result = await this.listFeatureFlagsUseCase.execute(searchParams);
+  @ApiOkResponse({
+    description: 'The FeatureFlag has been successfully created.',
+    type: FeatureFlagCollectionPresenter,
+  })
+  @ApiResponse({ status: 422, description: 'Unprocessable Entity' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @UseGuards(AuthGuard)
+  @Get()
+  async search(@Query() searchParams: ListFeatureFlagsDto) {
+    const result = await this.listFeatureFlagsUseCase.execute(searchParams);
 
-        return FeatureFlagController.listFeatureFlagToResponse(result);
-    }
+    return FeatureFlagController.listFeatureFlagToResponse(result);
+  }
 
-    @ApiBearerAuth()
-    @ApiResponse({status: 401, description: 'Unauthorized'})
-    @ApiResponse({status: 404, description: 'FeatureFlag not found'})
-    @UseGuards(AuthGuard)
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-        const output = await this.getFeatureFlagUseCase.execute({id});
+  @ApiBearerAuth()
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'FeatureFlag not found' })
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const output = await this.getFeatureFlagUseCase.execute({ id });
 
-        return FeatureFlagController.featureFlagToResponse(output);
-    }
+    return FeatureFlagController.featureFlagToResponse(output);
+  }
 
-    @ApiBearerAuth()
-    @ApiResponse({status: 401, description: 'Unauthorized'})
-    @ApiResponse({status: 422, description: 'Unprocessable Entity'})
-    @ApiResponse({status: 404, description: 'FeatureFlag not found'})
-    @UseGuards(AuthGuard)
-    @Put(':id')
-    async update(@Param('id') id: string, @Body() updateFeatureFlagDto: UpdateFeatureFlagDto) {
-        const output = await this.updateFeatureFlagUseCase.execute({
-            id,
-            ...updateFeatureFlagDto,
-        });
+  @ApiBearerAuth()
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 422, description: 'Unprocessable Entity' })
+  @ApiResponse({ status: 404, description: 'FeatureFlag not found' })
+  @UseGuards(AuthGuard)
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateFeatureFlagDto: UpdateFeatureFlagDto,
+  ) {
+    const output = await this.updateFeatureFlagUseCase.execute({
+      id,
+      ...updateFeatureFlagDto,
+    });
 
-        return FeatureFlagController.featureFlagToResponse(output);
-    }
+    return FeatureFlagController.featureFlagToResponse(output);
+  }
 
-    @ApiBearerAuth()
-    @ApiResponse({status: 401, description: 'Unauthorized'})
-    @ApiResponse({status: 404, description: 'FeatureFlag not found'})
-    @ApiResponse({status: 204, description: 'FeatureFlag deleted'})
-    @UseGuards(AuthGuard)
-    @HttpCode(204)
-    @Delete(':id')
-    async remove(@Param('id') id: string) {
-        await this.deleteFeatureFlagUseCase.execute({id});
-    }
+  @ApiBearerAuth()
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'FeatureFlag not found' })
+  @ApiResponse({ status: 204, description: 'FeatureFlag deleted' })
+  @UseGuards(AuthGuard)
+  @HttpCode(204)
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    await this.deleteFeatureFlagUseCase.execute({ id });
+  }
 }
