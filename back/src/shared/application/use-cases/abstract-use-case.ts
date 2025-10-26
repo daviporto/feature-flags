93deca225fa32e137ba2a98d1dba388ecf61c35a
davi <davi.araujo@onfly.com.br>
2025-10-26 16:10:20 -0300

    add assureRequiredInputProvided in abstract use case

--- back/src/shared/application/use-cases/abstract-use-case.ts
+++ back/src/shared/application/use-cases/abstract-use-case.ts
@@ -1,14 +1,18 @@
 import { BadRequestError } from '@/shared/application/errors/bad-request-error';
 import { UseCaseInterface } from '@/shared/application/use-cases/use-case';
+import { isUndefined } from '@nestjs/common/utils/shared.utils';
 
 export abstract class AbstractUseCase<Input, Output>
   implements UseCaseInterface<Input, Output>
 {
   abstract execute(input: Input): Promise<Output> | Output;
 
-  protected assureRequiredInputProvided(input: Input, requiredFields: string[]) {
+  protected assureRequiredInputProvided(
+    input: Input,
+    requiredFields: string[],
+  ) {
     requiredFields.forEach((field) => {
-      if (!input[field]) {
+      if (isUndefined(input[field])) {
         throw new BadRequestError(`${field} is required`);
       }
     });
