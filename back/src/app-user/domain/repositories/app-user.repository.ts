a65e9fb7166156513927d600147d32d2467de451
davi <davi.araujo@onfly.com.br>
2025-11-01 13:57:48 -0300

    add app user repository

---
+++ back/src/app-user/domain/repositories/app-user.repository.ts
@@ -0,0 +1,33 @@
+import { AppUserEntity } from '@/app-user/domain/entities/app-user.entity';
+import {
+  SearchableRepositoryInterface,
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+} from '@/shared/domain/repositories/searchable-repository-contracts';
+
+export namespace AppUserRepository {
+  export type Filter = {
+    name?: string;
+    email?: string;
+    externalId?: boolean;
+  };
+
+  export const sortableFields = ['name', 'email', 'externalId', 'createdAt'];
+
+  export const defaultSortField = 'createdAt';
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<
+    AppUserEntity,
+    Filter
+  > {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      AppUserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {}
+}
