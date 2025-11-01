fa9db3474721c666f526548d44c0a21d93fe28bc
davi <davi.araujo@onfly.com.br>
2025-11-01 19:25:00 -0300

    add update dto

---
+++ back/src/app-user/infrastructure/dtos/update-app-user.dto.ts
@@ -0,0 +1,24 @@
+import { UpdateAppUserUsecase } from '@/app-user/application/usecases/update-app-user.usecase';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateAppUserDto
+  implements Omit<UpdateAppUserUsecase.Input, 'id'>
+{
+  email: string;
+  @ApiProperty({
+    description: 'The name of the app user',
+    example: 'new app user',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'The app user external id',
+    example: '4859759348asds',
+  })
+  @IsNotEmpty()
+  @IsString()
+  externalId: string;
+}
