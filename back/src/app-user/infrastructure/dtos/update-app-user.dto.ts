6fc38a6758e5ed2a0267cd90039dc9abff77412f
davi <davi.araujo@onfly.com.br>
2025-11-01 19:41:15 -0300

    add decorators for email validation

--- back/src/app-user/infrastructure/dtos/update-app-user.dto.ts
+++ back/src/app-user/infrastructure/dtos/update-app-user.dto.ts
@@ -1,11 +1,18 @@
 import { UpdateAppUserUsecase } from '@/app-user/application/usecases/update-app-user.usecase';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateAppUserDto
   implements Omit<UpdateAppUserUsecase.Input, 'id'>
 {
+  @ApiProperty({
+    description: 'The email of the app user',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty({
     description: 'The name of the app user',
     example: 'new app user',
