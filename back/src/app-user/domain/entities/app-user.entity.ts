a9d643a0e6c9ff9c96f3deff95d065bafd66c02b
davi <davi.araujo@onfly.com.br>
2025-11-01 13:51:25 -0300

    add app user entity

---
+++ back/src/app-user/domain/entities/app-user.entity.ts
@@ -0,0 +1,59 @@
+import { Entity } from '@/shared/domain/entities/entity';
+import { AppUserValidatorFactory } from '@/app-user/domain/validators/app-user.validator';
+import { EntityValidationError } from '@/shared/domain/errors/validation-errors';
+
+export type AppUserProps = {
+  createdAt?: Date;
+  updatedAt?: Date;
+  externalId: string;
+  email: string;
+  name: string;
+};
+
+export class AppUserEntity extends Entity<AppUserProps> {
+  constructor(
+    public readonly props: AppUserProps,
+    id?: string,
+  ) {
+    AppUserEntity.validate(props);
+    super(props, id);
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  static validate(props: AppUserProps) {
+    const validator = AppUserValidatorFactory.create();
+    const isValid = validator.validate(props);
+
+    if (!isValid) {
+      throw new EntityValidationError(validator.errors);
+    }
+  }
+
+  get externalId(): string {
+    return this.props.externalId;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  update(externalId: string, email: string, name: string): void {
+    AppUserEntity.validate({ ...this.props, externalId, email, name });
+    this.props.externalId = externalId;
+    this.props.email = email;
+    this.props.name = name;
+    this.setUpdatedAt();
+  }
+
+  private setUpdatedAt(): void {
+    this.props.updatedAt = new Date();
+  }
+}
