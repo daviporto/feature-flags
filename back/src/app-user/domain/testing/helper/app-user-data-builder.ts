69ff6c41bb08e6520c446e999825ecf7dcdc0c00
davi <davi.araujo@onfly.com.br>
2025-11-01 18:50:16 -0300

    add default value for data builder

--- back/src/app-user/domain/testing/helper/app-user-data-builder.ts
+++ back/src/app-user/domain/testing/helper/app-user-data-builder.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { AppUserProps } from '@/app-user/domain/entities/app-user.entity';
 
-export function AppUserDataBuilder(props: Partial<AppUserProps>) {
+export function AppUserDataBuilder(props: Partial<AppUserProps> = {}) {
   return {
     name: props.name || faker.person.fullName(),
     email: props.email || faker.internet.email(),
